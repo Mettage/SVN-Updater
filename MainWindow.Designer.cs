f8dd53a2c7053c41d26d07cce64f512ee7e70d1f
Matej Molcan <matej.molcan@bagirasys.com>
2021-03-15 13:39:02 +0100

    Added minimize button
    Added minimize button

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,93 +28,92 @@ partial class MainWindow
     /// </summary>
     private void InitializeComponent()
     {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
-            this.SVNFolderTextBox = new System.Windows.Forms.TextBox();
-            this.BrowseButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.UpdateButton = new System.Windows.Forms.Button();
-            this.FoldersListBox = new System.Windows.Forms.ListBox();
-            this.SuspendLayout();
-            // 
-            // SVNFolderTextBox
-            // 
-            this.SVNFolderTextBox.Location = new System.Drawing.Point(12, 25);
-            this.SVNFolderTextBox.Name = "SVNFolderTextBox";
-            this.SVNFolderTextBox.Size = new System.Drawing.Size(156, 20);
-            this.SVNFolderTextBox.TabIndex = 0;
-            this.SVNFolderTextBox.TextChanged += new System.EventHandler(this.SVNFolderTextBox_TextChanged);
-            // 
-            // BrowseButton
-            // 
-            this.BrowseButton.Location = new System.Drawing.Point(174, 23);
-            this.BrowseButton.Name = "BrowseButton";
-            this.BrowseButton.Size = new System.Drawing.Size(75, 23);
-            this.BrowseButton.TabIndex = 1;
-            this.BrowseButton.Text = "Browse";
-            this.BrowseButton.UseVisualStyleBackColor = true;
-            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "SVN Folder:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 48);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Available SVN folders:";
-            // 
-            // UpdateButton
-            // 
-            this.UpdateButton.Location = new System.Drawing.Point(93, 247);
-            this.UpdateButton.Name = "UpdateButton";
-            this.UpdateButton.Size = new System.Drawing.Size(75, 23);
-            this.UpdateButton.TabIndex = 5;
-            this.UpdateButton.Text = "Update";
-            this.UpdateButton.UseVisualStyleBackColor = true;
-            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
-            // 
-            // FoldersListBox
-            // 
-            this.FoldersListBox.FormattingEnabled = true;
-            this.FoldersListBox.ImeMode = System.Windows.Forms.ImeMode.On;
-            this.FoldersListBox.Location = new System.Drawing.Point(12, 64);
-            this.FoldersListBox.Name = "FoldersListBox";
-            this.FoldersListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-            this.FoldersListBox.Size = new System.Drawing.Size(236, 173);
-            this.FoldersListBox.TabIndex = 6;
-            // 
-            // MainWindow
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Chocolate;
-            this.ClientSize = new System.Drawing.Size(260, 277);
-            this.Controls.Add(this.FoldersListBox);
-            this.Controls.Add(this.UpdateButton);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.BrowseButton);
-            this.Controls.Add(this.SVNFolderTextBox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "MainWindow";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "SVN Updater";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( MainWindow ) );
+        this.SVNFolderTextBox = new System.Windows.Forms.TextBox();
+        this.BrowseButton = new System.Windows.Forms.Button();
+        this.label1 = new System.Windows.Forms.Label();
+        this.label2 = new System.Windows.Forms.Label();
+        this.UpdateButton = new System.Windows.Forms.Button();
+        this.FoldersListBox = new System.Windows.Forms.ListBox();
+        this.SuspendLayout();
+        //
+        // SVNFolderTextBox
+        //
+        this.SVNFolderTextBox.Location = new System.Drawing.Point( 12, 25 );
+        this.SVNFolderTextBox.Name = "SVNFolderTextBox";
+        this.SVNFolderTextBox.Size = new System.Drawing.Size( 156, 20 );
+        this.SVNFolderTextBox.TabIndex = 0;
+        this.SVNFolderTextBox.TextChanged += new System.EventHandler( this.SVNFolderTextBox_TextChanged );
+        //
+        // BrowseButton
+        //
+        this.BrowseButton.Location = new System.Drawing.Point( 174, 23 );
+        this.BrowseButton.Name = "BrowseButton";
+        this.BrowseButton.Size = new System.Drawing.Size( 75, 23 );
+        this.BrowseButton.TabIndex = 1;
+        this.BrowseButton.Text = "Browse";
+        this.BrowseButton.UseVisualStyleBackColor = true;
+        this.BrowseButton.Click += new System.EventHandler( this.BrowseButton_Click );
+        //
+        // label1
+        //
+        this.label1.AutoSize = true;
+        this.label1.Location = new System.Drawing.Point( 12, 9 );
+        this.label1.Name = "label1";
+        this.label1.Size = new System.Drawing.Size( 64, 13 );
+        this.label1.TabIndex = 3;
+        this.label1.Text = "SVN Folder:";
+        //
+        // label2
+        //
+        this.label2.AutoSize = true;
+        this.label2.Location = new System.Drawing.Point( 12, 48 );
+        this.label2.Name = "label2";
+        this.label2.Size = new System.Drawing.Size( 112, 13 );
+        this.label2.TabIndex = 4;
+        this.label2.Text = "Available SVN folders:";
+        //
+        // UpdateButton
+        //
+        this.UpdateButton.Location = new System.Drawing.Point( 93, 247 );
+        this.UpdateButton.Name = "UpdateButton";
+        this.UpdateButton.Size = new System.Drawing.Size( 75, 23 );
+        this.UpdateButton.TabIndex = 5;
+        this.UpdateButton.Text = "Update";
+        this.UpdateButton.UseVisualStyleBackColor = true;
+        this.UpdateButton.Click += new System.EventHandler( this.UpdateButton_Click );
+        //
+        // FoldersListBox
+        //
+        this.FoldersListBox.FormattingEnabled = true;
+        this.FoldersListBox.ImeMode = System.Windows.Forms.ImeMode.On;
+        this.FoldersListBox.Location = new System.Drawing.Point( 12, 64 );
+        this.FoldersListBox.Name = "FoldersListBox";
+        this.FoldersListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+        this.FoldersListBox.Size = new System.Drawing.Size( 236, 173 );
+        this.FoldersListBox.TabIndex = 6;
+        //
+        // MainWindow
+        //
+        this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.BackColor = System.Drawing.Color.Chocolate;
+        this.ClientSize = new System.Drawing.Size( 260, 277 );
+        this.Controls.Add( this.FoldersListBox );
+        this.Controls.Add( this.UpdateButton );
+        this.Controls.Add( this.label2 );
+        this.Controls.Add( this.label1 );
+        this.Controls.Add( this.BrowseButton );
+        this.Controls.Add( this.SVNFolderTextBox );
+        this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        this.Icon = ( ( System.Drawing.Icon )( resources.GetObject( "$this.Icon" ) ) );
+        this.MaximizeBox = false;
+        this.Name = "MainWindow";
+        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        this.Text = "SVN Updater";
+        this.FormClosing += new System.Windows.Forms.FormClosingEventHandler( this.MainWindow_FormClosing );
+        this.ResumeLayout( false );
+        this.PerformLayout();
 
     }
 
